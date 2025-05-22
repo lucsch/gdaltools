b36dc92cd8667d5ba48aa42ef379686d1276f6f1
Lucien SCHREIBER <lucien.schreiber@terranum.ch>
2025-05-22 08:38:50 +0200

    Updating build script for GDAL 3.10.3

--- gdal_addo/conanfile.py
+++ gdal_addo/conanfile.py
@@ -4,21 +4,22 @@ from conans import ConanFile, CMake
 class gdal_addo(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     requires = [
-        "gdal/3.5.2@terranum-conan+gdal/stable",
+        "gdal/3.10.3@terranum-conan+gdal/stable",
         ]
 
     generators = "cmake", "gcc", "txt"
 
     # this isn't needed anymore with wxWidgets 3.2.0 (using GTK 3.0)
     def configure(self):
-            self.options["gdal"].with_curl = True # for xml support
-            self.options["gdal"].shared = True
-    #    if self.settings.os == "Linux":
-    #        self.options["wxwidgets"].webview = False # webview control isn't available on linux.
+        self.options["gdal"].with_curl = True # for xml support
+        self.options["gdal"].shared = True
+        if self.settings.os == "Linux":
+            self.options["gdal"].with_png = False
 
     def imports(self):
         self.copy("*.dll", dst="bin", src="bin")  # From bin to bin
         self.copy("*.dylib*", dst="bin", src="lib")  # From lib to bin
+        self.copy("*.so*", dst="bin", src="lib")  # From lib to bin
 
     def build(self):
         cmake = CMake(self)
--- gdal_translate/conanfile.py
+++ gdal_translate/conanfile.py
@@ -11,14 +11,15 @@ class gdal_translate(ConanFile):
 
     # this isn't needed anymore with wxWidgets 3.2.0 (using GTK 3.0)
     def configure(self):
-            self.options["gdal"].with_curl = True # for xml support
-            self.options["gdal"].shared = True
-    #    if self.settings.os == "Linux":
-    #        self.options["wxwidgets"].webview = False # webview control isn't available on linux.
+        self.options["gdal"].with_curl = True # for xml support
+        self.options["gdal"].shared = True
+        if self.settings.os == "Linux":
+            self.options["gdal"].with_png = False
 
     def imports(self):
         self.copy("*.dll", dst="bin", src="bin")  # From bin to bin
         self.copy("*.dylib*", dst="bin", src="lib")  # From lib to bin
+        self.copy("*.so*", dst="bin", src="lib")  # From lib to bin
 
     def build(self):
         cmake = CMake(self)
